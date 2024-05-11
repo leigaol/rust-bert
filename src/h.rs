dd4d5eef1c11e01c131cfba20cec983a71e07a96
Lei Gao <leigaol@amazon.com>
2024-05-11 16:42:52 -0700

    add bin

---
+++ src/h.rs
@@ -0,0 +1,19 @@
+
+use anyhow::Result;
+use rust_bert::pipelines::sentence_embeddings::{
+    SentenceEmbeddingsBuilder, SentenceEmbeddingsModelType,
+};
+
+pub fn main() -> anyhow::Result<()> {
+    // Set-up sentence embeddings model
+    let model = SentenceEmbeddingsBuilder::remote(SentenceEmbeddingsModelType::AllMiniLmL12V2)
+        .create_model()?;
+
+    // Define input
+    let sentences = ["this is an example sentence", "each sentence is converted"];
+
+    // Generate Embeddings
+    let embeddings = model.encode(&sentences)?;
+    println!("{embeddings:?}");
+    Ok(())
+}
