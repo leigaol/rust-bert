108b6db910037417baae9b7a13fa7da1113aa069
Lei Gao <leigaol@amazon.com>
2024-05-12 11:35:40 -0700

    debug model cannot run in async runtime

--- src/h.rs
+++ src/h.rs
@@ -1,19 +1,38 @@
-
-use anyhow::Result;
+#![deny(warnings)]
 use rust_bert::pipelines::sentence_embeddings::{
-    SentenceEmbeddingsBuilder, SentenceEmbeddingsModelType,
+    SentenceEmbeddingsBuilder, SentenceEmbeddingsModel, SentenceEmbeddingsModelType,
 };
 
-pub fn main() -> anyhow::Result<()> {
-    // Set-up sentence embeddings model
-    let model = SentenceEmbeddingsBuilder::remote(SentenceEmbeddingsModelType::AllMiniLmL12V2)
-        .create_model()?;
+use once_cell::sync::Lazy;
+use warp::Filter;
+use std::sync::Mutex;
 
-    // Define input
-    let sentences = ["this is an example sentence", "each sentence is converted"];
+use serde_derive::{Deserialize, Serialize};
+#[derive(Deserialize, Serialize)]
+struct Employee {
+    name: String,
+}
 
-    // Generate Embeddings
-    let embeddings = model.encode(&sentences)?;
-    println!("{embeddings:?}");
-    Ok(())
+static MODEL: Lazy<Mutex<SentenceEmbeddingsModel>> = Lazy::new(|| {
+    println!("loading model");
+    Mutex::new(SentenceEmbeddingsBuilder::remote(SentenceEmbeddingsModelType::AllMiniLmL12V2)
+        .create_model()
+        .unwrap())
+});
+
+#[tokio::main]
+async fn main() {
+    // GET /hello/warp => 200 OK with body "Hello, warp!"
+    let promote = warp::post()
+        .and(warp::path("employees"))
+        // Only accept bodies smaller than 16kb...
+        .and(warp::body::content_length_limit(1024 * 16))
+        .and(warp::body::json())
+        .map(|employee: Employee| {
+            let _ = MODEL.lock().unwrap().encode(&[&employee.name]).unwrap();
+            println!("g");
+            warp::reply::json(&employee)
+        });
+    
+    warp::serve(promote).run(([127, 0, 0, 1], 3030)).await
 }
